c706953ce52e957a3cf830ccf08ad746694e6df4
dk <dk@gmail.com>
2022-05-25 20:51:52 +0530

    create user with response entity and path implemented

--- src/main/java/com/in28minutes/rest/webservices/user/UserResouce.java
+++ src/main/java/com/in28minutes/rest/webservices/user/UserResouce.java
@@ -1,8 +1,11 @@
 package com.in28minutes.rest.webservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +25,11 @@ public class UserResouce {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }

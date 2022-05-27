b97f2a031738742fbf7a2f1d88453659f9088846
dk <dk@gmail.com>
2022-05-27 20:37:56 +0530

    versoning implemented in four approaches

---
+++ src/main/java/com/in28minutes/rest/webservices/versioning/Name.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Name {
+
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/versioning/PersonV1.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonV1 {
+    private String name;
+}
